3c750bc44229d72e915517c46f273ca480394f9a
Chesney Julian <chesney.julian724@gmail.com>
2023-06-22 16:07:53 -0400

    add initial files with npm packages and created questions array object

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
+const questions = [
+    {
+        message: "What is the text for your logo? (Can contain no more than three characters)",
+        name: "text",
+        default: "SVG",
+    },
+    {
+        message: "What would you like the text color to be? (Color may be keyword or hexadecimal number)",
+        name: "textColor",
+        default: "white",
+    },
+    {
+        message: "Choose a shape for your logo:",
+        name: "shape",
+        default: "circle",
+        type: "list",
+        choices: ["Square", "Circle", "Triangle"],
+    },
+    {
+        message: "What would you like the shape's color to be? (Color may be keyword or hexadecimal number)",
+        name: "shapeColor",
+        default: "blue",
+    },
+]
